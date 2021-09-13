1d982fce2c2c5986d42acc9fb72668681941511f
Majid Lotfinia <majid.lotfinia@mentorcliq.com>
2021-09-13 16:52:20 +0400

    add static data

---
+++ src/data.ts
@@ -0,0 +1,37 @@
+export const SLIDES = [
+  {
+    id: 'slide1',
+    image: require('./../assets/slide1.png'),
+    brand: 'CLESO',
+    price: '$220'
+  },
+  {
+    id: 'slide2',
+    image: require('./../assets/slide2.png'),
+    brand: 'HISAKO',
+    price: '$249'
+  },
+  {
+    id: 'slide3',
+    image: require('./../assets/slide3.png'),
+    brand: 'Avenir',
+    price: '$263'
+  }
+]
+
+export const PRODUCTS = [
+  {
+    id: 'product1',
+    image: require('./../assets/product1.png'),
+    brand: 'ORMOUS',
+    price: '$249',
+    size: 'White, size L'
+  },
+  {
+    id: 'product1',
+    image: require('./../assets/product1.png'),
+    brand: 'HISAKO',
+    price: '$249',
+    size: 'Black, size L'
+  }
+]
