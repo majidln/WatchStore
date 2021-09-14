c99c9afc7ff522252751474d297d396bf8abd38f
Majid Lotfinia <majid.lotfinia@mentorcliq.com>
2021-09-14 12:04:06 +0400

    feat: add zoom animation to images

--- src/screens/List.tsx
+++ src/screens/List.tsx
@@ -7,48 +7,56 @@ const { width, height } = Dimensions.get('screen')
 const SPACING = 40
 const SLIDE_HEIGHT = height * (2 / 3)
 const SLIDE_WIDTH = width - SPACING
+const nishhar = width - ((SLIDE_WIDTH + SPACING) * 2 + SPACING)
+
 
 const ListScreen = () => {
   const scrollX = useRef(new Animated.Value(0)).current
 
   return (
-        <SafeAreaView style={styles.wrapper}>
-            <StatusBar backgroundColor="#E5E5E5" barStyle="dark-content"></StatusBar>
-            <Animated.FlatList
-              data={SLIDES}
-              horizontal
-              showsHorizontalScrollIndicator={false}
-              keyExtractor={item => item.id}
-              style={styles.list}
-              snapToInterval={SLIDE_WIDTH}
-              pagingEnabled
-              renderItem={({ item, index }) => {
-                const inputRange = [(index - 1), index, (index + 1)].map(item => item * SLIDE_WIDTH)
-                const scale = scrollX.interpolate({
-                  inputRange,
-                  outputRange: [1, 1.01, 1]
-                })
-                return (
-                  <View style={[styles.itemWrapper, index === SLIDES.length - 1 ? {} : { paddingRight: SPACING / 2 }]}>
-                    <Animated.Image source={item.image} resizeMode="contain"
-                    style={[styles.image, { transform: [{ scale }] }]} />
-                    <View style={styles.contentWrapper}>
-                      <Text>
-                        {item.brand}
-                      </Text>
-                      <Text>
-                        {item.price}
-                      </Text>
-                    </View>
-                  </View>
-                )
-              }}
-              onScroll={Animated.event(
-                [{ nativeEvent: { contentOffset: { x: scrollX } } }],
-                { useNativeDriver: false }
-              )}
-            />
-        </SafeAreaView>
+    <SafeAreaView style={styles.wrapper}>
+      <StatusBar backgroundColor="#E5E5E5" barStyle="dark-content"></StatusBar>
+      <Animated.FlatList
+        data={SLIDES}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={item => item.id}
+        style={styles.list}
+        snapToInterval={SLIDE_WIDTH}
+        snapToAlignment={'start'}
+        pagingEnabled
+        renderItem={({ item, index }) => {
+          const inputRange = [(index - 1), index, (index + 1)].map(item => item * SLIDE_WIDTH)
+          const scale = scrollX.interpolate({
+            inputRange,
+            outputRange: [1, 1.2, 1]
+          })
+          return (
+            <View style={[
+              styles.itemWrapper,
+              index === SLIDES.length - 1 ? {} : { paddingRight: SPACING / 2 },
+            ]}>
+              <Animated.Image
+                source={item.image}
+                resizeMode="cover"
+                style={[styles.image, { transform: [{ scale }] }]} />
+              <View style={styles.contentWrapper}>
+                <Text style={styles.brand}>
+                  {item.brand}
+                </Text>
+                <Text style={styles.price}>
+                  {item.price}
+                </Text>
+              </View>
+            </View>
+          )
+        }}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+          { useNativeDriver: false }
+        )}
+      />
+    </SafeAreaView>
   )
 }
 
@@ -62,15 +70,29 @@ const styles = StyleSheet.create({
   },
   itemWrapper: {
     width: SLIDE_WIDTH,
-    height: SLIDE_HEIGHT
+    height: SLIDE_HEIGHT,
+    overflow: 'hidden',
+    zIndex: 1
   },
   image: {
-    width: '100%',
-    height: SLIDE_HEIGHT
+    width: SLIDE_WIDTH - SPACING,
+    height: SLIDE_HEIGHT,
+    borderRadius: 1
   },
   contentWrapper: {
     position: 'absolute',
-    bottom: 20
+    bottom: 40,
+    left: 20
+  },
+  brand: {
+    color: '#fff',
+    fontSize: 24,
+    fontFamily: 'Avenir'
+  },
+  price: {
+    color: '#fff',
+    fontSize: 20,
+    fontFamily: 'Avenir'
   }
 })
 
